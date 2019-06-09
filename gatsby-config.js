f64116381884c310c8f0c303821d817bb5b8391b
Unknown <gabriele@rigoinvestment.com>
2019-06-09 17:27:59 +0200

    commit pore website

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
-    title: "Gatsby Starter - Dimension V2",
-    author: "Hunter Chang",
+    title: "Popone Realty Group",
+    author: "Gabriele Rigo",
     description: "A Gatsby.js V2 Starter based on Dimension by HTML5 UP"
   },
   plugins: [
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,24 +1,26 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
+import prglogo from '../images/gatsby-icon.png'
+
 const Header = (props) => (
     <header id="header" style={props.timeout ? {display: 'none'} : {}}>
-        <div className="logo">
+        {/*<div className="logo">
             <span className="icon fa-diamond"></span>
-        </div>
+        </div>*/}
         <div className="content">
             <div className="inner">
-                <h1>Dimension</h1>
-                <p>A fully responsive site template designed by <a href="https://html5up.net">HTML5 UP</a> and released<br />
-                for free under the <a href="https://html5up.net/license">Creative Commons</a> license.</p>
+                <h1>Popone Realty Group</h1>
+                <p>Edilizia a scopo residenziale abitativo e direzionale.<br />Terreni edificabili in via Popone a Udine.
+                </p>
             </div>
         </div>
         <nav>
             <ul>
-                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('intro')}}>Intro</a></li>
-                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('work')}}>Work</a></li>
-                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('about')}}>About</a></li>
-                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('contact')}}>Contact</a></li>
+                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('intro')}}>Terreni</a></li>
+                {/*<li><a href="javascript:;" onClick={() => {props.onOpenArticle('work')}}>Progetti</a></li>*/}
+                {/*<li><a href="javascript:;" onClick={() => {props.onOpenArticle('about')}}>About</a></li>*/}
+                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('contact')}}>Contatti</a></li>
             </ul>
         </nav>
     </header>
